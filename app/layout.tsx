6063e40dd5238a758c248528013646cb82fe704c
imzany1 <sayedhusainabbashaider78600@gmail.com>
2025-12-16 13:02:56 +0530

    Refactor layouts, add product details page, and fix image fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { CartProvider } from "./context/CartContext";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Providers>
           <CartProvider>
             {children}
+            <Analytics />
           </CartProvider>
         </Providers>
       </body>
